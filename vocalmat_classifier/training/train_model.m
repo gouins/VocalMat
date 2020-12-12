1b8233d128a8738beb7fe01cb568ee09d9aa5ef7
Samir Gouin <65608691+gouins@users.noreply.github.com>
2020-12-12 10:03:43 -0500

    Add files via upload

--- vocalmat_classifier/training/train_model.m
+++ vocalmat_classifier/training/train_model.m
@@ -16,7 +16,9 @@ raiz    = pwd;
 model_class_DL  = load('Mdl_categorical_DL.mat');
 model_class_DL  = model_class_DL.netTransfer;
 
-images_training = imageDatastore([raiz '/.dataset/'],'IncludeSubfolders',true,'LabelSource','foldernames');
+images_training = imageDatastore('/Users/samirgouin/Library/Mobile Documents/com~apple~CloudDocs/Docs/Clarke Lab/VocalMat-master/Total (all batches)','IncludeSubfolders',true,'LabelSource','foldernames');
+
+images_training.ReadFcn = @customreader; %Preprocess photo (size & colour)
 tbl             = countEachLabel(images_training)
 Total           = sum(tbl.Count)
 minSetCount     = min(tbl{:,2}); % determine the smallest amount of images in a category
@@ -32,6 +34,7 @@ validationImages_labels = validationImages.Labels;
 
 save images_for_training trainingImages
 save images_for_validation validationImages
+
 
 miniBatchSize         = 128;
 numIterationsPerEpoch = floor(numel(trainingImages.Labels)/miniBatchSize);
@@ -54,7 +57,7 @@ layers          = [
                     softmaxLayer
                     classificationLayer];
 
-netTransfer     = trainNetwork(trainingImages,layers,options);
+netTransfer     = trainNetwork(images_training,layers,options);
 predictedLabels = classify(netTransfer,validationImages);
 
 % cd('F:\RF+DL\All_samples_noise')
@@ -67,25 +70,39 @@ ttt      = netTransfer.Layers(end).ClassNames;
 
 
 aux               = predictedLabels                  == validationImages.Labels;
-chevron_stats     = sum(validationImages.Labels(aux) == 'chevron')/sum(validationImages.Labels     == 'chevron')
-complex_stats     = sum(validationImages.Labels(aux) == 'complex')/sum(validationImages.Labels     == 'complex')
-down_fm_stats     = sum(validationImages.Labels(aux) == 'down_fm')/sum(validationImages.Labels     == 'down_fm')
+%chevron_stats     = sum(validationImages.Labels(aux) == 'chevron')/sum(validationImages.Labels     == 'chevron')
+trill_stats       = sum(validationImages.Labels(aux) == 'trill')/sum(validationImages.Labels     == 'trill')
+%flat_trill_stats  = sum(validationImages.Labels(aux) == 'flat_trill')/sum(validationImages.Labels     == 'flat_trill')
+%jump_trill_stats  = sum(validationImages.Labels(aux) == 'jump_trill')/sum(validationImages.Labels     == 'jump_trill')
+%Hz_stats          = sum(validationImages.Labels(aux) == 'Hz')/sum(validationImages.Labels     == 'Hz')
+%short_stats       = sum(validationImages.Labels(aux) == 'short')/sum(validationImages.Labels     == 'short')
+%split_stats       = sum(validationImages.Labels(aux) == 'split')/sum(validationImages.Labels     == 'split')
 flat_stats        = sum(validationImages.Labels(aux) == 'flat')/sum(validationImages.Labels        == 'flat')
-mult_steps_stats  = sum(validationImages.Labels(aux) == 'mult_steps')/sum(validationImages.Labels  == 'mult_steps')
-noise_dist_stats  = sum(validationImages.Labels(aux) == 'noise_dist')/sum(validationImages.Labels  == 'noise_dist')
-rev_chevron_stats = sum(validationImages.Labels(aux) == 'rev_chevron')/sum(validationImages.Labels == 'rev_chevron')
-short_stats       = sum(validationImages.Labels(aux) == 'short')/sum(validationImages.Labels       == 'short')
-step_down_stats   = sum(validationImages.Labels(aux) == 'step_down')/sum(validationImages.Labels   == 'step_down')
-step_up_stats     = sum(validationImages.Labels(aux) == 'step_up')/sum(validationImages.Labels     == 'step_up')
-two_steps_stats   = sum(validationImages.Labels(aux) == 'two_steps')/sum(validationImages.Labels   == 'two_steps')
-up_fm_stats       = sum(validationImages.Labels(aux) == 'up_fm')/sum(validationImages.Labels       == 'up_fm')
+other_stats       = sum(validationImages.Labels(aux) == 'other')/sum(validationImages.Labels     == 'other')
+%step_up_stats     = sum(validationImages.Labels(aux) == 'step_up')/sum(validationImages.Labels     == 'step_up')
+%step_down_stats   = sum(validationImages.Labels(aux) == 'step_down')/sum(validationImages.Labels     == 'step_down')
+%multi_step_stats  = sum(validationImages.Labels(aux) == 'multi_step')/sum(validationImages.Labels     == 'multi_step')
+%up_ramp_stats     = sum(validationImages.Labels(aux) == 'up_ramp')/sum(validationImages.Labels     == 'up_ramp')
+%down_ramp_stats   = sum(validationImages.Labels(aux) == 'down_ramp')/sum(validationImages.Labels     == 'down_ramp')
+
+%mult_steps_stats  = sum(validationImages.Labels(aux) == 'mult_steps')/sum(validationImages.Labels  == 'mult_steps')
+%noise_dist_stats  = sum(validationImages.Labels(aux) == 'noise_dist')/sum(validationImages.Labels  == 'noise_dist')
+%rev_chevron_stats = sum(validationImages.Labels(aux) == 'rev_chevron')/sum(validationImages.Labels == 'rev_chevron')
+%short_stats       = sum(validationImages.Labels(aux) == 'short')/sum(validationImages.Labels       == 'short')
+%step_down_stats   = sum(validationImages.Labels(aux) == 'step_down')/sum(validationImages.Labels   == 'step_down')
+%complex_stats     = sum(validationImages.Labels(aux) == 'complex')/sum(validationImages.Labels     == 'complex')
+%down_fm_stats     = sum(validationImages.Labels(aux) == 'down_fm')/sum(validationImages.Labels     == 'down_fm')
+%two_steps_stats   = sum(validationImages.Labels(aux) == 'two_steps')/sum(validationImages.Labels   == 'two_steps')
+%up_fm_stats       = sum(validationImages.Labels(aux) == 'up_fm')/sum(validationImages.Labels       == 'up_fm')
 
 % T_out2 = [table_total2(:,[1 2]), table_total2(:,239), array2table(ynewci_RF,'VariableNames',Mdl.ClassNames), array2table(scores,'VariableNames',s'),  array2table(ynew_RF,'VariableNames',{'RF'}),  array2table(predictedLabels,'VariableNames',{'DL'}), table_total2(:,[238])];             
 %%
 %testing the trained net
 disp('Testing the trained network on all the samples used for training...')
-images                    = imageDatastore([pwd '/.dataset/'],'IncludeSubfolders',true);
-% images.ReadFcn = @(loc)imresize(imread(loc),[224 224]);
+images = imageDatastore('/Users/samirgouin/Library/Mobile Documents/com~apple~CloudDocs/Docs/Clarke Lab/VocalMat-master/Total (all batches)','IncludeSubfolders',true);
+images.ReadFcn = @customreader;
+
+%images.ReadFcn = @(loc)imresize(imread(loc),[224 224]);
 [predictedLabels, scores] = classify(netTransfer,images);
 
 T = [cell2table(images_training.Files,'VariableNames',{'Training_file'}) cell2table(cellstr(images_training.Labels),'VariableNames',{'Training_label'})...
@@ -135,4 +152,13 @@ function stop = stopIfAccuracyNotImproving(info,N)
         
     end
 
+end
+
+
+function data = customreader(filename)
+onState = warning('off', 'backtrace');
+c = onCleanup(@() warning(onState));
+data=imread(filename);
+data = imresize(data, [227 227]);
+data=data(:,:,min(1:3,end)); 
 end
